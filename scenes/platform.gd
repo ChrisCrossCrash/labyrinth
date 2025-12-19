67ab9ca72b4925ed9b41ae7f2303a51674501dfe
Christopher Kumm <chrislkumm@protonmail.com>
2025-12-19 08:45:38 -0600

    Fix `tilt_rate` export not being used

--- scenes/platform.gd
+++ scenes/platform.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
 @export var angle_max_deg := 20.0
-@export var tilt_rate := 3.0
+@export var tilt_rate := 2.0
 
 func _process(delta: float) -> void:
     var input_joy := Input.get_vector("forward", "backward", "right", "left")
     var target_x_rad := deg_to_rad(input_joy.x * angle_max_deg)
     var target_y_rad := deg_to_rad(input_joy.y * angle_max_deg)
     var rotation_target := Vector3(target_x_rad, 0.0, target_y_rad)
-    rotation = rotation.lerp(rotation_target, 3.0 * delta)
+    rotation = rotation.lerp(rotation_target, tilt_rate * delta)
